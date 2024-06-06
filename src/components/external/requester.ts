a501b1ce5cc94e7f90e47ca27c5c69e5bce74b25
Handeye <58805641+JollyRojer1512@users.noreply.github.com>
2024-06-06 09:43:10 +0500

    make requester to throw an error instead of doing nothing

--- src/components/external/requester.ts
+++ src/components/external/requester.ts
@@ -1,27 +1,32 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import { Injectable } from '@nestjs/common';
+import { Logger } from '../utils/logger';
+import { LogLevel } from '../config/types';
 
 @Injectable()
 export class Requester {
 	private readonly client: AxiosInstance;
+	private readonly logger = new Logger({ logLevel: LogLevel.info });
 	constructor() {
 		this.client = axios.create();
 	}
 
-	async get<R>(url: string, options?: AxiosRequestConfig): Promise<R | undefined> {
+	async get<R>(url: string, options?: AxiosRequestConfig): Promise<R> {
 		try {
 			const response = await this.client.get<R>(url, options);
 			return response.data;
 		} catch (e) {
-			return;
+			this.logger.error('Error in Requester.get: ', e);
+			throw e;
 		}
 	}
-	async post<R>(url: string, body: Record<string, any>, options?: AxiosRequestConfig): Promise<R | undefined> {
+	async post<R>(url: string, body: Record<string, any>, options?: AxiosRequestConfig): Promise<R> {
 		try {
 			const response = await this.client.post<R>(url, body, options);
 			return response.data;
 		} catch (e) {
-			return;
+			this.logger.error('Error in Requester.post: ', e);
+			throw e;
 		}
 	}
 }
